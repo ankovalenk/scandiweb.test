6e0024f3ae66de6bde2e356de178a15f0d4fac71
ankovalenk <107536924+ankovalenk@users.noreply.github.com>
2024-04-19 12:22:43 +0300

    Add files via upload

---
+++ Actions/process_product.php
@@ -0,0 +1,7 @@
+<?php
+require_once(__DIR__."/../Classes/product.php");
+require_once(__DIR__."/../Classes/dvd.php");
+require_once(__DIR__."/../Classes/book.php");
+require_once(__DIR__."/../Classes/furniture.php");
+
+$product = Product::createProduct();
